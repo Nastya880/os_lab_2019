ec3f5c5aca44aec3473fcc2df68e3b80aa155ccc
nastya_makeeva <oidmakeevy@gmail.com>
2020-03-03 18:20:34 +0000

    nomer1

--- lab2/src/swap/main.c
+++ lab2/src/swap/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 #include "swap.h"
-
+#include "swap.c"
 int main()
 {
 	char ch1 = 'a';
 	char ch2 = 'b';
 
-	Swap(&ch1, &ch2);
+	swap(&ch1, &ch2);
 
 	printf("%c %c\n", ch1, ch2);
 	return 0;
