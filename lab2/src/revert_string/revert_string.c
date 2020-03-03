d0fee3523ea1ec912925a6ab3a59519a008f6339
nastya_makeeva <oidmakeevy@gmail.com>
2020-03-03 19:37:11 +0000

    nomer2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,21 @@
 #include "revert_string.h"
+#include<stdio.h>
+#include<string.h>
 
-void RevertString(char *str)
-{
-	// your code here
+void RevertString(char *str){
+    register int i, len = 0;
+    //Определяем длину строки
+    len = strlen(str);
+    //Выполняем реверс символов в строке
+    for (i = 0; i <= len / 2; i++){
+        *(str + len - i) = *(str + i);  
+        *(str + i) = *(str + len - i - 1);
+    } 
+
+    //Сдвигаем вторую половину символов влево на 1 символ
+    for (i = len / 2; i <= len; i++)
+        *(str + i) = *(str + i + 1);
+        
+    //Устанавливаем символ завершения строки
+    *(str + len) = '\0';
 }
-
