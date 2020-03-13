a316793d5a3e897214bb2685085a4897447daddc
nastya_makeeva <oidmakeevy@gmail.com>
2020-03-13 11:46:53 +0000

    nomer4 in lab1 sdelan

--- lab1/src/average.sh
+++ lab1/src/average.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 count=0
 summa=0
-for param in "$@"
+for param in $(cat numbs.txt) 
 do
 count=$(($count+1))
 summa=$(($summa+$param))
 done
 echo "quality $count"
 b=$(($summa/$count))
+#echo summa "$summa"
 echo srednee "$b"
